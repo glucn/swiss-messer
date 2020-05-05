8fb69ea8fe9b509cca7d2b52685b5b5a6b3e6075
Gary Lu <madcated.lu@gmail.com>
2020-05-04 23:00:21 -0700

    Follow along amChart4 MapChart example

--- src/app/json-pretty-print/json-pretty-print.component.ts
+++ src/app/json-pretty-print/json-pretty-print.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4maps from '@amcharts/amcharts4/maps';
+import am4geodata_worldLow from '@amcharts/amcharts4-geodata/worldLow';
 
 @Component({
   templateUrl: './json-pretty-print.component.html',
@@ -8,5 +11,29 @@ export class JsonPrettyPrintComponent implements OnInit {
 
 
   ngOnInit(): void {
+    const map = am4core.create('chartdiv', am4maps.MapChart);
+    map.geodata = am4geodata_worldLow;
+    map.projection = new am4maps.projections.Miller();
+
+    const polygonSeries = new am4maps.MapPolygonSeries();
+    polygonSeries.useGeodata = true;
+    polygonSeries.exclude = ['AQ'];
+    map.series.push(polygonSeries);
+
+    const polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.fill = am4core.color('#74B266');
+
+    const lineSeries = map.series.push(new am4maps.MapLineSeries());
+    lineSeries.data = [{
+      'multiGeoLine': [
+        [
+          { 'latitude': 48.856614, 'longitude': 2.352222 },
+          { 'latitude': 40.712775, 'longitude': -74.005973 },
+          { 'latitude': 49.282729, 'longitude': -123.120738 }
+        ]
+      ]
+    }];
+
+
   }
 }
